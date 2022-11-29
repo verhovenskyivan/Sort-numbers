707979eb0bebadffc75a2140a287ebbcf5d11956
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-11-29 21:05:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Kata
+{
+    public static int[] SortNumbers(int[] nums)
+    {
+        if (nums == null)
+            nums = new int[0];
+
+        Array.Sort(nums);
+        return nums;
+    }
+}
